d5a81574e420f47b4e8cef2ca903bf34a3218af2
Jithendra123892 <jithendramaddala009988@gmail.com>
2025-06-28 12:22:28 +0530

    Completed Task 2 - Integrated H2 DB and added status endpoint

---
+++ Task2/src/main/java/com/telstra/sim/ActuatorResponse.java
@@ -0,0 +1,14 @@
+package com.telstra.sim;
+
+public class ActuatorResponse {
+    private boolean success;
+
+    // Getter and setter
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
---
+++ Task2/src/main/java/com/telstra/sim/SimActivationRepository.java
@@ -0,0 +1,6 @@
+package com.telstra.sim;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SimActivationRepository extends JpaRepository<SimActivation, Long> {
+}
